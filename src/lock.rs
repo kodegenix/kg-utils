0032448996f9d80758807b4d43f848b3ecd0d514
jchlapinski <jakub.chlapinski@kodegenix.pl>
2019-10-08 14:40:17 +0200

    syncref dev

--- src/lock.rs
+++ src/lock.rs
@@ -1,30 +1,24 @@
-use super::*;
-
 use std::sync::Arc;
-use std::cell::{RefCell, Ref, RefMut};
 use std::ops::{Deref, DerefMut};
 
-use parking_lot::{ReentrantMutex, ReentrantMutexGuard, MappedReentrantMutexGuard};
-use std::borrow::Borrow;
+use parking_lot::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 
 
-pub struct SyncRef<T>(Arc<ReentrantMutex<RefCell<T>>>);
+pub struct SyncRef<T>(Arc<RwLock<T>>);
 
 impl<T> SyncRef<T> {
     pub fn new(value: T) -> SyncRef<T> {
-        SyncRef(Arc::new(ReentrantMutex::new(RefCell::new(value))))
+        SyncRef(Arc::new(RwLock::new(value)))
     }
 
-    pub fn read<'a>(&'a self) -> SyncRefReadGuard<'a, T> {
-        let g = self.0.lock();
-        let r = unsafe { std::mem::transmute::<Ref<'_, _>, Ref<'a, _>>(g.deref().borrow()) };
-        SyncRefReadGuard { r, g }
+    #[inline(always)]
+    pub fn read(&self) -> SyncRefReadGuard<T> {
+        SyncRefReadGuard(self.0.read())
     }
 
-    pub fn write<'a>(&'a self) -> SyncRefWriteGuard<'a, T> {
-        let g = self.0.lock();
-        let r = unsafe { std::mem::transmute::<RefMut<'_, _>, RefMut<'a, _>>(g.deref().borrow_mut()) };
-        SyncRefWriteGuard { r, g }
+    #[inline(always)]
+    pub fn write(&self) -> SyncRefWriteGuard<T> {
+        SyncRefWriteGuard(self.0.write())
     }
 }
 
@@ -33,36 +27,30 @@ unsafe impl<T> Send for SyncRef<T> {}
 unsafe impl<T> Sync for SyncRef<T> {}
 
 
-pub struct SyncRefReadGuard<'a, T> {
-    r: Ref<'a, T>,
-    g: ReentrantMutexGuard<'a, RefCell<T>>,
-}
+pub struct SyncRefReadGuard<'a, T>(RwLockReadGuard<'a, T>);
 
 impl<'a, T> Deref for SyncRefReadGuard<'a, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
-        self.r.deref()
+        self.0.deref()
     }
 }
 
 
-pub struct SyncRefWriteGuard<'a, T> {
-    r: RefMut<'a, T>,
-    g: ReentrantMutexGuard<'a, RefCell<T>>,
-}
+pub struct SyncRefWriteGuard<'a, T>(RwLockWriteGuard<'a, T>);
 
 impl<'a, T> Deref for SyncRefWriteGuard<'a, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
-        self.r.deref()
+        self.0.deref()
     }
 }
 
 impl<'a, T> DerefMut for SyncRefWriteGuard<'a, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        self.r.deref_mut()
+        self.0.deref_mut()
     }
 }
 
